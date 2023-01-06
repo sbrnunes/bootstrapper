086e7e8e8594987fbaf8272d59bbe741651243da
Sergio Nunes <sbrnunes@gmail.com>
2023-01-06 00:00:54 +0000

    Adding config file

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,10 @@ info() {
 
 main() {
   info "Bootstrapping this machine";
-  /bin/bash -c ./bootstrappers/bootstrap-xcode.sh;
-  /bin/bash -c ./bootstrappers/bootstrap-iterm.sh;
-  /bin/bash -c ./bootstrappers/bootstrap-homebrew.sh;
-  /bin/bash -c ./bootstrappers/bootstrap-zsh.sh;
+
+  while read -r bootstrapper; do
+    /bin/bash -c ./bootstrappers/bootstrap-$bootstrapper.sh;
+  done < config
 }
 
 main "$@";
