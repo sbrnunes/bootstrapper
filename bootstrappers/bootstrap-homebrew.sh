e12bf3a9514efd33e0d6f237ad5b7344df809172
Sergio Nunes <sbrnunes@gmail.com>
2023-01-06 04:12:10 +0000

    Fixing script

--- bootstrappers/bootstrap-homebrew.sh
+++ bootstrappers/bootstrap-homebrew.sh
@@ -19,18 +19,18 @@ add_brew_to_path() {
 }
 
 main() {
-  info "Downloading and updating brew"; # -------------------------------------------------------------
-
   SUDO_USER=$(whoami);
 
   if ! type -t brew; then
-    info "Downloading and installing Homebrew";
+    info "Downloading and installing brew"; # -------------------------------------------------------------
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     if [ ! $? -eq 0 ] 
     then
         info "Failed to install Homebrew"
         exit 1
     fi
+  else
+    info "Homebrew already installed!"
   fi;
 
   echo 'PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile;
