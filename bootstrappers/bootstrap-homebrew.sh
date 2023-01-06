66a888be6808eb4648eab186dd0e93f10dde7042
Sergio Nunes <sbrnunes@gmail.com>
2023-01-06 01:28:05 +0000

    Running in non interactive

--- bootstrappers/bootstrap-homebrew.sh
+++ bootstrappers/bootstrap-homebrew.sh
@@ -19,7 +19,7 @@ main() {
 
   if ! type -t brew; then
     >&2 echo "Downloading and installing Homebrew";
-    chsh -s "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi;
 
   echo 'PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
